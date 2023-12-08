306a448c31ba8f41b01e07c5a80511a64b731bf6
OpesMentis <planchot.antoine@gmail.com>
2023-12-08 11:08:44 +0100

    Day 7, Part 2

--- day7.nim
+++ day7.nim
@@ -4,28 +4,45 @@ import tables
 import math
 import algorithm
 
-proc eval_hand(hand: string): int =
-    const kinds = "23456789TJQKA"
+proc eval_hand(hand: string, part: int): int =
+    const kinds = ["23456789TJQKA", "J23456789TQKA"]
+    const pow13_5 = 13^5
+
     let cards = hand.toCountTable()
+    let lg = len(cards)
     let max_occ = cards.largest()[1]
 
     for c in hand:
         result *= 13
-        result += kinds.find(c)
+        result += kinds[part-1].find(c)
 
-    if len(cards) == 1:
-        result += 6 * 13^5
-    elif len(cards) == 2 and max_occ == 4:
-        result += 5 * 13^5
-    elif len(cards) == 2:
-        result += 4 * 13^5
-    elif len(cards) == 3 and max_occ == 3:
-        result += 3 * 13^5
-    elif len(cards) == 3:
-        result += 2 * 13^5
-    elif len(cards) == 4:
-        result += 13^5
+    if part == 1 or 'J' notin cards:
+        if lg == 1:
+            result += 6 * pow13_5
+        elif lg == 2 and max_occ == 4:
+            result += 5 * pow13_5
+        elif lg == 2:
+            result += 4 * pow13_5
+        elif lg == 3 and max_occ == 3:
+            result += 3 * pow13_5
+        elif lg == 3:
+            result += 2 * pow13_5
+        elif lg == 4:
+            result += pow13_5
+        
+        return result
 
+    if lg <= 2:
+        result += 6 * pow13_5
+    elif lg == 3 and cards['J'] == 1 and max_occ == 2:
+        result += 4 * pow13_5
+    elif lg == 3:
+        result += 5 * pow13_5
+    elif lg == 4:
+        result += 3 * pow13_5
+    else:
+        result += pow13_5
+    
     return result
 
 let p = initOptParser()
@@ -40,23 +57,34 @@ let data = f.readAll().strip().split("\n")
 f.close()
 
 var bids: Table[string, int]
-var vals: OrderedTable[string, int]
+var vals1: OrderedTable[string, int]
+var vals2: OrderedTable[string, int]
 var info: seq[string]
 var hand: string
 var score: int
-var i = 1
 
 for line in data:
     info = line.strip().split(" ")
     hand = info[0]
 
     bids[hand] = parseInt(info[1])
-    vals[hand] = eval_hand(hand)
+    vals1[hand] = eval_hand(hand, 1)
+    vals2[hand] = eval_hand(hand, 2)
 
-vals.sort(proc (x, y: (string, int)): int = cmp(x[1], y[1]))
+vals1.sort(proc (x, y: (string, int)): int = cmp(x[1], y[1]))
+vals2.sort(proc (x, y: (string, int)): int = cmp(x[1], y[1]))
 
-for h in vals.keys:
+var i = 1
+for h in vals1.keys:
     score += i * bids[h]
     i += 1
 
 echo "Part 1: ", score
+
+score = 0
+i = 1
+for h in vals2.keys:
+    score += i * bids[h]
+    i += 1
+
+echo "Part 2: ", score
